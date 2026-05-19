14eb4ad17824e3bb3993d79a183faf306f0f8023
MidnightCoding <Calimali2006@gmail.com>
2026-05-19 13:55:08 +0200

    Add framebuffer request and end marker to main.rs

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,6 +4,15 @@
 mod panic;
 
 use core::panic::PanicInfo;
+use limine::request::FramebufferRequest;
+
+#[used]
+#[link_section = ".requests"]
+static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[link_section = ".requests"]
+static END_MARKER: [u64; 2] = [0, 0];
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
