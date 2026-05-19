50f415c4921e39a9cc7a61fbbfa73650e963e713
MidnightCoding <Calimali2006@gmail.com>
2026-05-19 15:58:17 +0200

    Add timer and keyboard interrupt handlers to IDT
    Added handlers for timer and keyboard interrupts to the IDT.

--- kernel/src/interrupts/idt.rs
+++ kernel/src/interrupts/idt.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::InterruptDescriptorTable;
 
 use crate::interrupts::handlers;
+use crate::interrupts::keyboard_irq;
 
 static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
@@ -11,6 +12,12 @@ pub fn init_idt() {
         IDT.page_fault.set_handler_fn(handlers::page_fault_handler);
         IDT.general_protection_fault.set_handler_fn(handlers::gpf_handler);
 
+        IDT[32].set_handler_fn(timer_interrupt);
+        IDT[33].set_handler_fn(keyboard_irq::keyboard_interrupt_handler);
+
         IDT.load();
     }
+}
+
+extern "x86-interrupt" fn timer_interrupt(_stack_frame: x86_64::structures::idt::InterruptStackFrame) {
 }
