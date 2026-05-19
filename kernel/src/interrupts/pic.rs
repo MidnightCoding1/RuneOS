6f1bf39f36eb8e8a022a5788570b6a46c8e2accb
MidnightCoding <Calimali2006@gmail.com>
2026-05-19 20:14:25 +0200

    Rename variables for PIC command clarity

--- kernel/src/interrupts/pic.rs
+++ kernel/src/interrupts/pic.rs
@@ -2,22 +2,25 @@ use x86_64::instructions::port::Port;
 
 const PIC1_COMMAND: u16 = 0x20;
 const PIC1_DATA: u16 = 0x21;
+
 const PIC2_COMMAND: u16 = 0xA0;
 const PIC2_DATA: u16 = 0xA1;
 
 const ICW1_INIT: u8 = 0x10;
 const ICW1_ICW4: u8 = 0x01;
+
 const ICW4_8086: u8 = 0x01;
 
 pub fn init_pic() {
     unsafe {
-        let mut pic1_cmd = Port::new(PIC1_COMMAND);
+        let mut pic1_command = Port::new(PIC1_COMMAND);
         let mut pic1_data = Port::new(PIC1_DATA);
-        let mut pic2_cmd = Port::new(PIC2_COMMAND);
+
+        let mut pic2_command = Port::new(PIC2_COMMAND);
         let mut pic2_data = Port::new(PIC2_DATA);
 
-        pic1_cmd.write(ICW1_INIT | ICW1_ICW4);
-        pic2_cmd.write(ICW1_INIT | ICW1_ICW4);
+        pic1_command.write(ICW1_INIT | ICW1_ICW4);
+        pic2_command.write(ICW1_INIT | ICW1_ICW4);
 
         pic1_data.write(0x20);
         pic2_data.write(0x28);
