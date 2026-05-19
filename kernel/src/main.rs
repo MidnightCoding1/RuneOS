0fd0f334a917862500540bfbecb8d4959887f347
MidnightCoding <Calimali2006@gmail.com>
2026-05-19 14:01:28 +0200

    Set up framebuffer and clear with specified color
    Initialize framebuffer and clear it with a color.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
+mod framebuffer;
 mod panic;
 
-use core::panic::PanicInfo;
+use framebuffer::writer;
 use limine::request::FramebufferRequest;
 
 #[used]
@@ -16,5 +17,16 @@ static END_MARKER: [u64; 2] = [0, 0];
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let framebuffer_response = FRAMEBUFFER_REQUEST
+        .get_response()
+        .unwrap();
+
+    let framebuffer = framebuffer_response
+        .framebuffers()
+        .next()
+        .unwrap();
+
+    writer::clear(framebuffer, 0x202020);
+
     loop {}
 }
